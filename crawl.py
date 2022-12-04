e216ddb3a855c03f74b9861905f798f97161b7ee
lingranshanye2357 <2357143181@qq.com>
2022-12-04 10:03:02 +0800

    更新爬虫代码

--- crawl.py
+++ crawl.py
@@ -1,169 +1,91 @@
 import requests
 import time
 from bs4 import BeautifulSoup
-from url_normalize import url_normalize
-from urllib.parse import urljoin
 import numpy as np
 import re
+import os
 
+
+# 本次的任务和之前那个不一样，这次不需要获取网页链接，东西应该都是现成的
 class Crawler(object):
-    def __init__(self, start_tasks=[], encoding=None, headers={'user-agent': 'my-app/0.0.1'},
-                 wait_time=0.05, timeout=5, save_path='D:/courses/检索/作业/'):
-        # 设置全局变量
-        self.visited = []
-        self.queue = start_tasks  # 队列,list
-
-        # self.visited = np.load('./visited_list.npy', allow_pickle=True).tolist()  # set
-        # self.queue = np.load('./list.npy', allow_pickle=True).tolist()
-        # 基本变量
+    def __init__(self, crawler_list=[], encoding='utf-8', headers={'user-agent': 'my-app/0.0.1'},
+                 wait_time=3, timeout=5, save_path='D:/courses/检索/lofter/', tag_num=1000,
+                 base_url='https://www.lofter.com/tag/', mode='total', margin=500):
+        self.list = crawler_list    # 爬取序列
         self.encoding = encoding
         self.headers = headers
         self.wait_time = wait_time
         self.timeout = timeout
         self.save_path = save_path
+        self.tag_num = tag_num   # 每个tag爬多少个
+        self.base_url = base_url
+        self.mode = mode        # 爬取模式：全部/月榜/日榜
+        self.margin = margin    # 字数限制，一般字数比较多的文章，质量比较高，相关性强
 
-    # 网页规范化，爬取不同网页时应该有不同的设置
-    def check(self, url, u):
-        if 'pdf' in u:
-            return None
-        if 'download' in u:
-            return None
-        if 'doc' in u:
-            return None
-        if 'xlsx' in u:
-            return None
-        if 'xls' in u:
-            return None
-        if 'jpg' in u:
-            return None
-        if 'rar' in u:
-            return None
-        if 'zip' in u:
-            return None
-        if 'upload' in u:
-            return None
-        if '附件' in u:
-            return None
-        if 'ico' in u:
-            return None
-        if 'css' in u:
-            return None
-        if 'uploads' in u:
-            return None
-        if 'http' not in u:   # 如果不是绝对路径
-            u = urljoin(url, u)
-            u = url_normalize(u)
-            if 'http://ctd.ruc.edu.cn' in u:
-                if 'http://ctd.ruc.edu.cn/web/' not in u:
-                    u = u.replace('http://ctd.ruc.edu.cn/', 'http://ctd.ruc.edu.cn/web/')
-                    return u
-            if 'http://fangxue.ruc.edu.cn' in u:
-                if 'http://fangxue.ruc.edu.cn/web/' not in u:
-                    u = u.replace('http://fangxue.ruc.edu.cn/', 'http://fangxue.ruc.edu.cn/web/')
-                    return u
-        if 'ruc.edu.cn' in u:  # 检查是否为人大域名下的
-            return u
-        else:
-            return None    #
+    def crawler(self):
+        for tag in self.list:    # 逐个处理tag
+            url = self.base_url + tag + '/' + self.mode
+            next_page = url
+            text_path = self.save_path + '文本/' + tag + '/'
+            html_path = self.save_path + '网页/' + tag + '/'
+            work_num = 1   # 记录当前tag下爬取的文章数量
+            page_num = 1   # 记录当前翻页的数量
+            count = 0  # 记录没爬到文章的累积页数
 
-    def get_html(self, url):
-        try:
-            r = requests.get(url, headers=self.headers, timeout=3)
-            if r.status_code == 404:
-                return None
-            if self.encoding is not None:
-                r.encoding = self.encoding
-            return r.text
-        except:
-            return None
+            # 先创建文件夹
+            if not os.path.exists(text_path):  # 如果该路径不存在
+                os.makedirs(text_path)  # 创造文件夹
+            # 创建网页文件夹
+            if not os.path.exists(html_path):
+                os.makedirs(html_path)
 
-    # 得到一个网页中的所有链接
-    def get_url(self, html_text):
-        temp_set = set()  # 用集合的方式，防止放入很多重复的
-        soup = BeautifulSoup(html_text, 'html.parser')   # 创建一个类
-        # soup = BeautifulSoup(html_text, features="lxml-xml")
-        for tag in soup.find_all('a'):   # 找到所有的a
-            url = tag.attrs.get('href', None)
-            if url is not None and url != '':
-                temp_set.add(url)
-        # for tag in soup.find_all('link'):
-        #     url = tag.attrs.get('href', None)
-        #     if url is not None and url != '':
-        #         temp_set.add(url)
-        for tag in soup.find_all('script'):
-            a = str(tag)
-            if 'href' in a:
-                pattern = re.compile("\'(.*?)\'", re.S)  # 表达式为: (.*?)
-                url = pattern.findall(a)
-            else:
-                url = None
-            if url is not None:
+            title_txt = open(text_path + '题目.txt', 'a', encoding=self.encoding)
+
+
+            while True:
+                # 每次循环，分析当前的网页
                 try:
-                    temp_set.add(url[0])
+                    r = requests.get(url=next_page, headers=self.headers, timeout=self.timeout)
                 except:
-                    continue
-        return temp_set
+                    return None
+                # 分析该网页的源代码
+                html_text = r.text
+                soup = BeautifulSoup(html_text, 'html.parser')
+                # 提取当前网页中的长文章题目与内容
+                titles = soup.find_all('h2', class_="tit")  # 这个是长文章的题目
+                contents = soup.find_all('div', class_="txt js-content ptag")  # 这个是长文章的内容
+                # 依次提取并保存
+                if len(contents) == 0:
+                    count += 1
+                    print(count)
+                if count > 10:  # 连续十页没爬到东西
+                    break
+                for k in range(len(contents)):   # 一共有len(titles)个长文本
+                    title = titles[k].getText()
+                    content = contents[k]
 
-    # 得到一个html的正文
-    def get_text(self, html_text):
-        soup = BeautifulSoup(html_text, 'html.parser')
-        # 提取并处理正文与标题
-        if soup.title is not None:  # 如果不是空的，就存title
-            title = soup.title.string
-        else:
-            title = None  # 否则title=None
+                    # 将文章写入
+                    with open(text_path + str(work_num) + '.txt', 'w', encoding=self.encoding) as f:
+                        f.write(title)
+                        f.write('\n')
+                        text = '\n'.join([p.text for p in content.find_all('p') if p.text is not None])
+                        f.write(text)
+                    work_num += 1   # 每保存一篇文章，work数量加一
+                    print('tag: {}, work{}: {}'.format(tag, work_num, title))
+                    title_txt.write(title)
+                    title_txt.write('\n')
 
-        text = '\n'.join([p.text for p in soup.find_all('p') if p.text is not None])  # 得到正文
-        return title, text
+                with open(html_path + str(page_num) + '.html', 'w', encoding=self.encoding) as f:
+                    f.write(html_text)
+                print('page: {}'.format(page_num))
+                page_num += 1  # 每处理完一页，页数加一
 
-    # 爬取
-    def crawl(self):
-        count = 0   # count用于记录当前的总爬取次数，并为保存的内容命名
-        while len(self.queue) > 0:
-            url = self.queue.pop(0)  # 弹出第一个
-            if 'linkedin' in url:
-                continue
-            if url in self.visited:
-                continue
-            html_text = self.get_html(url)  # 第一个函数：得到网页内容
-            if html_text == '' or html_text is None:
-                continue
-            temp_set = self.get_url(html_text)   # 第二个函数：得到网页链接
-            for u in temp_set:
-                u = self.check(url, u)  # 第三个函数：判断是否已经爬取过
-                if (u not in self.queue) and (u not in self.visited) and (u is not None):
-                    self.queue.append(u)  # 队列任务加一
-
-            count += 1   # 保存当前网页和正文，count用于计数
-            path1 = self.save_path + '网页' + '/' + str(count) + '.html'  # 路径1，存网页
-            # path2 = self.save_path + '文本' + '/' + str(count) + '.txt'  # 路径2，存正文
-            # path3 = self.save_path + '源码' + '/' + str(count) + '.txt'
-
-            # title, text = self.get_text(html_text)  # 第三个函数：提取标题和正文
-            with open(path1, 'w', encoding='utf-8') as f:  # 保存网页
-                f.write(html_text)
-
-            # with open(path2, 'w', encoding='utf-8') as f:  # 保存正文
-            #     if title is not None:
-            #         f.write(title)
-            #     f.write('\n')
-            #     if text is not None:
-            #         f.write(text)
-            #
-            # with open(path3, 'w', encoding='utf-8') as f:  # 保存源码
-            #     f.write(html_text)
-
-            # 先保存正文、网页再加入url，保证一一对应
-            print(count, url)
-            self.visited.append(url)  # 加入到已访问当中
-            np.save(self.save_path + 'list.npy', list(self.queue))  # 保存当前队列，为了下次能从结束的位置开始
-            np.save(self.save_path + 'visited_list.npy', list(self.visited))  # 保存已经访问过的网址
-            with open(self.save_path + 'html.txt', 'a', newline='') as f:
-                f.write(url)
-                f.write('\n')
-
-            time.sleep(self.wait_time)  # 每爬取一个网页，需要停顿几秒
+                if work_num >= self.tag_num:
+                    break
+                # 当前网页的长文章处理完之后，要获取下一页的url
+                next_page = url + '?page=' + str(page_num)
+            title_txt.close()
 
 
-
-a = Crawler(['http://www.ruc.edu.cn/'], encoding='utf-8')
+a = Crawler(['ibsm', '云次方', '少爷和我', 'mamamoo', '格林威治', '楼诚', '', '', ''])
+a.crawler()
